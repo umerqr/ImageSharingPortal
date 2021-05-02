0dbdc0789af992e2a7981f211ad444bbd33e6dec
Umer Qureshi <umerqr@gmail.com>
2021-05-02 18:47:09 +0500

    cleaned up api code

--- backEnd/routes/users/functions.js
+++ backEnd/routes/users/functions.js
@@ -13,31 +13,7 @@ const middleWareForVerifyToken = (incomingToken, incomingFunction) => {
     }
   });
 };
-const fetchUser = async (req, res) => {
-  try {
-    const { body } = req;
-    const { email, password } = body;
-    const users = userDataset.find(
-      (x) => x.email === email && x.password === password
-    );
 
-    if (users) {
-      jwt.sign({ user: users }, `secretkey`, (err, token) => {
-        return res.json({
-          token,
-          userInfo: users,
-        });
-      });
-    } else {
-      res.sendStatus(404);
-    }
-  } catch (e) {
-    console.log(`error occured`);
-  }
-  //   finally {
-  //     res.json(responseObj.result);
-  //   }
-};
 const verifyToken = (req, res, next) => {
   const bearerHeader = req.headers['token'];
   if (typeof bearerHeader !== 'undefined') {
@@ -49,7 +25,48 @@ const verifyToken = (req, res, next) => {
   }
 };
 
+const fetchUser = async (req, res) => {
+  let responseObj = {};
+  try {
+    const { body } = req;
+    const { email, password } = body;
+    const users = userDataset.find(
+      (x) => x.email === email && x.password === password
+    );
+
+    if (users) {
+      await jwt.sign({ user: users }, `secretkey`, (err, token) => {
+        responseObj = {
+          status: 200,
+          result: {
+            token,
+            userInfo: users,
+          },
+        };
+        res.status(responseObj.status).json(responseObj.result);
+      });
+    } else {
+      responseObj = {
+        status: 404,
+        result: {
+          message: `not found`,
+        },
+      };
+      res.status(responseObj.status).json(responseObj.result);
+    }
+  } catch (e) {
+    responseObj = {
+      status: 404,
+      result: {
+        message: `not found`,
+      },
+    };
+    res.status(responseObj.status).json(responseObj.result);
+  }
+};
+
 const fetchData = async (req, res) => {
+  let responseObj = {};
   try {
     const imageData = imageDataset.items;
     const { token } = req.headers;
@@ -64,14 +81,32 @@ const fetchData = async (req, res) => {
         }
       });
       if (imageData) {
-        res.json({
-          items: filtedData,
-        });
+        responseObj = {
+          status: 200,
+          result: {
+            items: filtedData,
+          },
+        };
+      } else {
+        responseObj = {
+          status: 404,
+          result: {
+            message: `No data found.`,
+          },
+        };
       }
     };
     return middleWareForVerifyToken(token, fetchDataSubFunction);
   } catch (e) {
+    responseObj = {
+      status: 500,
+      result: {
+        message: `Error, unable to perform action, Server error.`,
+      },
+    };
     console.log(`error occured`);
+  } finally {
+    res.status(responseObj.status).json(responseObj.result);
   }
 };
 const fetchUserData = async (req, res) => {
@@ -91,25 +126,44 @@ const fetchUserData = async (req, res) => {
     };
     return middleWareForVerifyToken(token, fetchUserDataSubFunction);
   } catch (e) {
+    responseObj = {
+      status: 500,
+      result: {
+        message: `Error, unable to perform action, Server error.`,
+      },
+    };
     console.log(`error occured`);
   }
 };
 const fetchUserInfo = async (req, res) => {
+  let responseObj = {};
   const { token } = req.headers;
   try {
     const fetchUserInfoSubFunction = (authData) => {
-      res.json({
-        userInfo: authData.user,
-        token,
-      });
+      responseObj = {
+        status: 200,
+        result: {
+          userInfo: authData.user,
+          token,
+        },
+      };
     };
     return middleWareForVerifyToken(token, fetchUserInfoSubFunction);
   } catch (e) {
+    responseObj = {
+      status: 500,
+      result: {
+        message: `Error, unable to perform action, Server error.`,
+      },
+    };
     console.log(`error occured`);
+  } finally {
+    res.status(responseObj.status).json(responseObj.result);
   }
 };
 
 const postUserImage = async (req, res) => {
+  let responseObj = {};
   const { token } = req.headers;
   const { body } = req;
   const { items } = body;
@@ -131,14 +185,32 @@ const postUserImage = async (req, res) => {
             }
           }
         );
-        res.sendStatus(200);
+        responseObj = {
+          status: 200,
+          result: {
+            message: `Successfully updated.`,
+          },
+        };
       } else {
-        res.sendStatus(500);
+        responseObj = {
+          status: 500,
+          result: {
+            message: `Error, unable to perform action, Server error.`,
+          },
+        };
       }
     };
     return middleWareForVerifyToken(token, postUserImageSubFunction);
   } catch (e) {
+    responseObj = {
+      status: 500,
+      result: {
+        message: `Error, unable to perform action, Server error.`,
+      },
+    };
     console.log(`error occured`);
+  } finally {
+    res.status(responseObj.status).json(responseObj.result);
   }
 };
 
