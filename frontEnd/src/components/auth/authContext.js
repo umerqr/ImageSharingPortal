37c9c025ea5fdbbb6146dacf4d223c3ead860e00
Umer Qureshi <umerqr@gmail.com>
2021-05-02 17:12:28 +0500

    fixed a few things

--- frontEnd/src/components/auth/authContext.js
+++ frontEnd/src/components/auth/authContext.js
@@ -1,11 +1,11 @@
 import { createContext } from 'react';
 
 export const AuthContext = createContext({
-  authenticated: false, // to check if authenticated or not
-  user: {}, // store all the user details
-  accessToken: ``, // accessToken of user for
-  handleLogin: () => {}, // handle login process
-  logout: () => {}, // handle login process
+  authenticated: false,
+  user: {},
+  accessToken: ``,
+  handleLogin: () => {},
+  logout: () => {},
 });
 export const AuthConsumer = AuthContext.Consumer;
 export const AuthProvider = AuthContext.Provider;
