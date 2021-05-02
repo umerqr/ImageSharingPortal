1dd48e0b06bacddf5dd037e039465709cf022dfb
Umer Qureshi <umerqr@gmail.com>
2021-05-02 18:12:29 +0500

    fix(chore): updated some functions

--- backEnd/routes/users/functions.js
+++ backEnd/routes/users/functions.js
@@ -4,6 +4,15 @@ const imageDataset = require(`./availableData.json`);
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
+const middleWareForVerifyToken = (incomingToken, incomingFunction) => {
+  jwt.verify(incomingToken, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      incomingFunction(authData);
+    }
+  });
+};
 const fetchUser = async (req, res) => {
   try {
     const { body } = req;
@@ -44,28 +53,23 @@ const fetchData = async (req, res) => {
   try {
     const imageData = imageDataset.items;
     const { token } = req.headers;
-    jwt.verify(token, 'secretkey', (err, authData) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        const {
-          user: { email },
-        } = authData;
-        const userDataset = userImageDataset.find((x) => x.email === email)
-          .items;
-
-        const filtedData = imageData.filter((x) => {
-          if (!userDataset.some((y) => y.id === x.id)) {
-            return x;
-          }
-        });
-        if (imageData) {
-          res.json({
-            items: filtedData,
-          });
+    const fetchDataSubFunction = (authData) => {
+      const {
+        user: { email },
+      } = authData;
+      const userDataset = userImageDataset.find((x) => x.email === email).items;
+      const filtedData = imageData.filter((x) => {
+        if (!userDataset.some((y) => y.id === x.id)) {
+          return x;
         }
+      });
+      if (imageData) {
+        res.json({
+          items: filtedData,
+        });
       }
-    });
+    };
+    return middleWareForVerifyToken(token, fetchDataSubFunction);
   } catch (e) {
     console.log(`error occured`);
   }
@@ -73,22 +77,19 @@ const fetchData = async (req, res) => {
 const fetchUserData = async (req, res) => {
   try {
     const { token } = req.headers;
-    jwt.verify(token, 'secretkey', (err, authData) => {
-      if (err) {
-        res.sendStatus(403);
+    const fetchUserDataSubFunction = (authData) => {
+      const imageData = userImageDataset.find(
+        (x) => x.email === authData.user.email
+      );
+      if (imageData) {
+        res.json({
+          items: imageData.items,
+        });
       } else {
-        const imageData = userImageDataset.find(
-          (x) => x.email === authData.user.email
-        );
-        if (imageData) {
-          res.json({
-            items: imageData.items,
-          });
-        } else {
-          res.sendStatus(404);
-        }
+        res.sendStatus(404);
       }
-    });
+    };
+    return middleWareForVerifyToken(token, fetchUserDataSubFunction);
   } catch (e) {
     console.log(`error occured`);
   }
@@ -96,16 +97,13 @@ const fetchUserData = async (req, res) => {
 const fetchUserInfo = async (req, res) => {
   const { token } = req.headers;
   try {
-    jwt.verify(token, 'secretkey', (err, authData) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        res.json({
-          userInfo: authData.user,
-          token,
-        });
-      }
-    });
+    const fetchUserInfoSubFunction = (authData) => {
+      res.json({
+        userInfo: authData.user,
+        token,
+      });
+    };
+    return middleWareForVerifyToken(token, fetchUserInfoSubFunction);
   } catch (e) {
     console.log(`error occured`);
   }
@@ -116,32 +114,29 @@ const postUserImage = async (req, res) => {
   const { body } = req;
   const { items } = body;
   try {
-    jwt.verify(token, 'secretkey', (err, authData) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        if (items) {
-          fs.readFile(
-            './routes/users/data.json',
-            'utf8',
-            function readFileCallback(err, data) {
-              if (err) {
-              } else {
-                let obj = JSON.parse(data); //now it an object
-                obj.find((x) => x.email === authData.user.email).items = items; //add some data
-                let json = JSON.stringify(obj); //convert it back to json
-                fs.writeFile('./routes/users/data.json', json, function (err) {
-                  if (err) throw err;
-                }); // write it back
-              }
+    const postUserImageSubFunction = (authData) => {
+      if (items) {
+        fs.readFile(
+          './routes/users/data.json',
+          'utf8',
+          function readFileCallback(err, data) {
+            if (err) {
+            } else {
+              let obj = JSON.parse(data); //now it an object
+              obj.find((x) => x.email === authData.user.email).items = items; //add some data
+              let json = JSON.stringify(obj); //convert it back to json
+              fs.writeFile('./routes/users/data.json', json, function (err) {
+                if (err) throw err;
+              }); // write it back
             }
-          );
-          res.sendStatus(200);
-        } else {
-          res.sendStatus(500);
-        }
+          }
+        );
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(500);
       }
-    });
+    };
+    return middleWareForVerifyToken(token, postUserImageSubFunction);
   } catch (e) {
     console.log(`error occured`);
   }
