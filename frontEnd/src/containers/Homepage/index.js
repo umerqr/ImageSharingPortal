ae069346f0595c70eea689f4c129342c26b8bb6a
umerqr <47629923+umerqr@users.noreply.github.com>
2021-09-14 21:34:08 +0100

    add routing and dashboard

--- frontEnd/src/containers/Homepage/index.js
+++ frontEnd/src/containers/Homepage/index.js
@@ -1,51 +1,88 @@
 import { Drawer } from '@material-ui/core';
-import React, { lazy, useState } from 'react';
+import React, { lazy, useEffect, useState } from 'react';
 import { Suspense } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useLocation, useHistory } from 'react-router-dom';
 import TopBar from '../../components/TopBar';
 import './styles.css';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import {
+  BookOutlined,
+  DashboardOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
 const ContentHomepage = lazy(() => import(`../ContentHomepage`));
 // const NotFoundPage = lazy(() => import(`../NotFoundPage`));
 // import PropTypes from 'prop-types';
 
 function Homepage() {
   //   const {} = props;
-  const drawerItems = [{ label: 'Dashboard', id: 0 }];
+  const drawerItems = [
+    {
+      label: 'Dashboard',
+      id: 0,
+      icon: <DashboardOutlined />,
+    },
+    { label: 'Library', id: 1, icon: <BookOutlined /> },
+    { label: 'Users', id: 2, icon: <UserOutlined /> },
+  ];
+  const [selectedDrawerItem, setSelectedDrawerItem] = useState(null);
   const [isDrawerOpen, setIsDrawerOpen] = useState(true);
+  let location = useLocation();
+  let history = useHistory();
+
+  useEffect(() => {
+    switch (location) {
+      case `/`:
+        setSelectedDrawerItem('Dashboard');
+        break;
+      default:
+        break;
+    }
+  }, []);
+  const onClickDrawerItem = (listItem) => {
+    const { label } = listItem;
+    if (label === selectedDrawerItem) {
+      setSelectedDrawerItem(label);
+    } else {
+      setSelectedDrawerItem(label);
+      history.push(label);
+    }
+  };
   const list = () => (
     <div role='presentation'>
       <List>
-        {drawerItems.map((item, index) => (
-          <ListItem button key={item.id}>
+        {drawerItems.map((item) => (
+          <ListItem
+            onClick={() => onClickDrawerItem(item)}
+            button
+            key={item.id}
+            alignItems='center'
+            className={
+              selectedDrawerItem === item.label
+                ? 'list-item-container list-item-selected '
+                : 'list-item-container'
+            }
+          >
             <ListItemIcon>
-              {index % 2 === 0 ? (
-                <span>
-                  <InboxIcon></InboxIcon>
+              <span>
+                <span className='drawer-label-icon'>{item.icon}</span>
+                <span
+                  className={`pl-1 drawer-list-label ${
+                    selectedDrawerItem === item.label &&
+                    'drawer-list-label-selected'
+                  }`}
+                >
                   {isDrawerOpen && item.label}
                 </span>
-              ) : (
-                <MailIcon />
-              )}
+              </span>
             </ListItemIcon>
           </ListItem>
         ))}
       </List>
       <Divider />
-      <List>
-        {['All mail'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
-          </ListItem>
-        ))}
-      </List>
     </div>
   );
   return (
@@ -68,8 +105,8 @@ function Homepage() {
       >
         <Switch>
           <Suspense fallback={<div>Loading...</div>}>
-            <Route path='/homepage' component={ContentHomepage} />
-            <Route path='/' component={ContentHomepage} />
+            <Route path='/dashboard' component={ContentHomepage} />
+            <Route exact path='/' component={ContentHomepage} />
           </Suspense>
         </Switch>
       </div>
